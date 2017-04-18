f98c810a395919d9c6d87c42a99a832f02ae276f
Brian Lee <blees.email@gmail.com>
2017-04-18 03:16:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
